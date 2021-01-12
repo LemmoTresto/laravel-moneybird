31031cedca53e7d143339c101a8bdcb83ad8b2f9
Max Berkelmans <maxberkelmans@Live.nl>
2021-01-12 16:51:02 +0100

    Added documentation and removed magic method handling to let laravel handle this

--- src/MoneybirdFacade.php
+++ src/MoneybirdFacade.php
@@ -3,33 +3,51 @@ namespace LemmoTresto\Moneybird;
 
 use Illuminate\Support\Facades\Facade as IlluminateFacade;
 
+/**
+ * @method static \Picqer\Financials\Moneybird\Entities\Administration administration(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\Contact contact(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\ContactCustomField contactCustomField(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\Note note(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\CustomField customField(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\DocumentStyle documentStyle(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\Estimate estimate(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\ExternalSalesInvoice externalSalesInvoice(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\ExternalSalesInvoiceDetail externalSalesInvoiceDetail(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\ExternalSalesInvoicePayment externalSalesInvoicePayment(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\FinancialAccount financialAccount(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\FinancialMutation financialMutation(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\GeneralDocument financialStatement(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\GeneralJournalDocument generalJournalDocument(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\GeneralJournalDocumentEntry generalJournalDocumentEntry(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\Identity identity(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\ImportMapping importMapping(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\LedgerAccount ledgerAccount(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\Product product(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\PurchaseInvoice purchaseInvoice(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\PurchaseInvoiceDetail purchaseInvoiceDetail(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\PurchaseInvoicePayment purchaseInvoicePayment(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\RecurringSalesInvoice recurringSalesInvoice(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\RecurringSalesInvoiceCustomField recurringSalesInvoiceCustomField(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\RecurringSalesInvoiceDetail recurringSalesInvoiceDetail(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\SalesInvoice salesInvoice(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\SalesInvoiceCustomField salesInvoiceCustomField(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\SalesInvoiceDetail salesInvoiceDetail(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\SalesInvoicePayment salesInvoicePayment(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\SalesInvoiceReminder salesInvoiceReminder(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\TaxRate taxRate(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\TimeEntry timeEntry(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\TypelessDocument typelessDocument(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\User user(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\Webhook webhook(array $attributes = [])
+ * @method static \Picqer\Financials\Moneybird\Entities\Workflow workflow(array $attributes = [])
+ *
+ * Class MoneybirdFacade
+ * @package LemmoTresto\Moneybird
+ */
 class MoneybirdFacade extends IlluminateFacade {
+
     protected static function getFacadeAccessor()
     {
         return 'moneybird';
-    }
-    public static function __callStatic($method, $args)
-    {
-        $instance = static::getFacadeRoot();
-        switch (count($args))
-        {
-            case 0:
-                return $instance->$method();
-
-            case 1:
-                return $instance->$method($args[0]);
-
-            case 2:
-                return $instance->$method($args[0], $args[1]);
-
-            case 3:
-                return $instance->$method($args[0], $args[1], $args[2]);
-
-            case 4:
-                return $instance->$method($args[0], $args[1], $args[2], $args[3]);
-
-            default:
-                return call_user_func_array(array($instance, $method), $args);
-        }
     }
 }
